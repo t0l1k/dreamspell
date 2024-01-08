21e780357c728bcf5c4014a54ae22ad387590c8c
Anatoli Tjushka <t0l1k@mail.ru>
2024-01-08 13:31:22 +0200

    readme update, app bg set to white

--- ui/app/app.go
+++ ui/app/app.go
@@ -1,12 +1,15 @@
 package app
 
 import (
+	"image/color"
+
 	"github.com/t0l1k/eui"
 )
 
 func NewGame() *eui.Ui {
 	u := eui.GetUi()
 	u.SetTitle("Dreamspell")
-	u.SetSize(640, 400)
+	u.SetSize(800, 600)
+	u.GetTheme().Set(eui.SceneBg, color.White)
 	return u
 }
